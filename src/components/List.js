6b8a67be12d3ce8218bed1f8bdc2ffa1f4df26b6
zahrabola <95353397+zahrabola@users.noreply.github.com>
2023-06-22 16:41:15 +0100

    Update list

--- src/components/List.js
+++ src/components/List.js
@@ -1,8 +1,22 @@
+import { CircularProgress, FormControl, InputLabel, MenuItem, Select, Typography } from '@mui/material';
+import React, { useState, useEffect, createRef } from 'react';
 const List = () => {
     return (
-<div>
-    list
-</div>
+<ListContainer>
+    <Typography> Restaurant, Cafes and Bars near you </Typography>
+    <ListLoading>
+        <CircularProgress />
+    </ListLoading>
+    <FormControl>
+        <InputLabel>Type</InputLabel>
+        <Select id='type' value={type}>
+            <MenuItem value="restaurants">Restaurants</MenuItem>
+            <MenuItem value="hotels">Hotels</MenuItem>
+            <MenuItem value="attractions">Attractions</MenuItem>
+        </Select>
+    </FormControl>
+    {/* */}
+</ListContainer>
     )
 }
 export default List
