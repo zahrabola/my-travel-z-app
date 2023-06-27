e50f8ca51615758df5c242fda12409e62a1c2e15
zahrabola <95353397+zahrabola@users.noreply.github.com>
2023-06-27 13:31:48 +0100

    Update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import React, { /*useState, useEffect  */} from 'react';
+import React, { useState, useEffect} from 'react';
 import './App.css';
 import Header from './components/Header';
 import List from './components/List';
 import Map from './components/Map';
 import CssBaseline from '@mui/material/CssBaseline';
 import { Grid } from '@mui/material';
+import {GetPlacesData} from './Data/TravelApi';
+
 
 function App() {
+
+  /* useEffect for Travel and weather */
+  useEffect( () => {
+    GetPlacesData();
+  }, []);
+
+
   return (
     <div className="App">
       <React.Fragment>
---
+++ src/Data/TravelApi.js
@@ -0,0 +1,51 @@
+
+////https://rapidapi.com/apidojo/api/travel-advisor?utm_source=youtube.com%2FJavaScriptMastery&utm_medium=DevRel&utm_campaign=DevRel 
+/* 
+resturant list in boundary - 
+*/
+/* copies fromrapid api */ 
+import axios from 'axios';
+const URLtravel = 'https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary'
+
+const options = {
+  /*
+  from rapid api
+  method: 'GET',
+  url: 'https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary', */
+  params: {
+    bl_latitude: '11.847676',
+    tr_latitude: '12.838442',
+    bl_longitude: '109.095887',
+    tr_longitude: '109.149359',
+    restaurant_tagcategory_standalone: '10591',
+    restaurant_tagcategory: '10591',
+    limit: '30',
+    currency: 'USD',
+    open_now: 'false',
+    lunit: 'km',
+    lang: 'en_US'
+  },
+  headers: {
+    'X-RapidAPI-Key': '8213bd53bbmsheef81f1d6808d94p1a2e3djsnc7301be69b83',
+    'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com'
+  }
+};
+
+/*
+from rapid api
+try {
+	const response = await axios.request(options);
+	console.log(response.data);
+} catch (error) {
+	console.error(error);
+}*/
+
+export const GetPlacesData = async () => {
+    try {
+        const {data: {data}} = await axios.get(URLtravel, options);
+        console.log(response.data);
+        return data;
+    } catch (error) {
+        console.error(error);
+    }
+}
